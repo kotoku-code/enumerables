ae8a1601ee0baee67aa7cc5cebe7baf3e6d8aede
kotoku-code <kennedy.kotoku@gmail.com>
2020-03-20 16:47:35 +0100

    made all,none,any methods

--- enumerable.rb
+++ enumerable.rb
@@ -16,6 +16,40 @@ result
     end
 
     def my_select
+        result=[]
+        my_each do |x|
+            output.push(x) if yield x
+        end
+        result
+    end
+
+    def my_all
+        result = true
+        my_each do |x|
+            result = false unless yield x
+        end
+        result
+    end
+
+    def my_any?
+        result = false
+        my_each do |x|
+            result = false if yield x
+        end 
+        result
+    end
+
+    def my_none?
+        result = true
+        my_each do |x|
+            result = false if yield x
+        end
+        result
+    end
+
+    
+
+
 
 
 
