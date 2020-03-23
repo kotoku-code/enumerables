b4069f29415a2c32e5edbc4529c3bbfe9e9557d5
kotoku-code <kennedy.kotoku@gmail.com>
2020-03-23 14:23:56 +0100

    added stickler and rubocop

--- enumerable.rb
+++ enumerable.rb
@@ -1,95 +1,87 @@
 module Enumerable
-    def my_each(arr)
-result=[]
-for i in 0..length
-    result.push(yield(arr[i]))
-end
-result
+  def my_each(arr)
+    result = []
+    for i in 0..length
+      result.push(yield(arr[i]))
     end
- 
-    def my_each_with_index(arr)
-        result = []
-        for i in 0..length
-            result.push(yield(arr[i],i))
-        end
-        result
-    end
+    result
+  end
 
-    def my_select
-        result=[]
-        my_each do |x|
-            output.push(x) if yield x
-        end
-        result
+  def my_each_with_index(arr)
+    result = []
+    for i in 0..length
+      result.push(yield(arr[i], i))
     end
+    result
+  end
 
-    def my_all
-        result = true
-        my_each do |x|
-            result = false unless yield x
-        end
-        result
+  def my_select
+    result = []
+    my_each do |x|
+      output.push(x) if yield x
     end
+    result
+  end
 
-    def my_any?
-        result = false
-        my_each do |x|
-            result = false if yield x
-        end 
-        result
+  def my_all
+    result = true
+    my_each do |x|
+      result = false unless yield x
     end
+    result
+  end
 
-    def my_none?
-        result = true
-        my_each do |x|
-            result = false if yield x
-        end
-        result
+  def my_any?
+    result = false
+    my_each do |x|
+      result = false if yield x
     end
+    result
+  end
 
-    def my_count
-        count = 0
-        my_each do |x|
-            count +=1 if yield x
-        end
-        count
+  def my_none?
+    result = true
+    my_each do |x|
+      result = false if yield x
     end
+    result
+  end
 
-    def my_map(arr)
-        result = []
-        my_each(arr) do |x|
-            result.push(yield(x))
-        end
-        result
+  def my_count
+    count = 0
+    my_each do |x|
+      count += 1 if yield x
     end
+    count
+  end
 
-    def my_map(&block)
-        new_result = []
-        self.my_each do |x|
-            new_result.push(block.call(i))
-        end
-        new_result
+  def my_map(arr)
+    result = []
+    my_each(arr) do |x|
+      result.push(yield(x))
     end
+    result
+  end
 
-    
-def my_inject (initial=0)
-    i=0
+  def my_map(&block)
+    new_result = []
+    self.my_each do |x|
+      new_result.push(block.call(i))
+    end
+    new_result
+  end
+
+  def my_inject(initial = 0)
+    i = 0
     total = initial
     while (i < self.length)
-        total = yield(total, self[i])
-        i = i +1
-    end 
+      total = yield(total, self[i])
+      i = i + 1
+    end
     total
-end
+  end
 
-def multiply_els
-    self.my_inject(1) {|total, n|total * n}
-end
-
-
-
-
-
-
-
+  def multiply_els
+    self.my_inject(1) { |total, n| total * n }
+  end
 end
