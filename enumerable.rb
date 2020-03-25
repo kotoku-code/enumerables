2e268bdd827e4862a36fffbde0c9d6573f5ade2e
kotoku-code <kennedy.kotoku@gmail.com>
2020-03-25 19:57:08 +0100

    fixed some of the linter issues

--- enumerable.rb
+++ enumerable.rb
@@ -1,124 +1,122 @@
-module Enumerable
-  def my_each(block)
-    result = []
-    return to_enum unless block_given?
+# frozen_string_literal: true
 
-    for i in 0..length
-      result.push(yield(block[i]))
-    end
-    result
+module_Enumerable
+def my_each(block)
+  result = []
+  return to_enum unless block_given?
+
+  (0..length).each do |i|
+    result.push(yield(block[i]))
+  end
+  result
+end
+
+def my_each_with_index(block)
+  result = []
+  return to_enum unless block_given?
+
+  (0..length).each do |i|
+    result.push(yield(block[i], i))
+  end
+  result
+end
+
+def my_select
+  result = []
+  return to_enum unless block_given?
+
+  my_each do |x|
+    output.push(x) if yield x
+  end
+  result
+end
+
+def my_all?
+  result = true
+  my_each do |x|
+    result = false unless yield x
+  end
+  result
+end
+if pattern.is_a? Class
+  my_each do |x|
+    return false unless x.is_a? pattern
+  end
+  true
+end
+
+def my_any?
+  result = false
+  my_each do |x|
+    result = false if yield x
+  end
+  result
+end
+if pattern.is_a? Class
+  my_each do |x|
+    return false unless x.is_a? pattern
+  end
+  true
+end
+
+def my_none?
+  result = true
+  my_each do |x|
+    result = false if yield x
+  end
+  result
+end
+if pattern.is_a? Class
+  my_each do |x|
+    return false unless x.is_a? pattern
+  end
+  true
+end
+
+def my_count
+  count = 0
+  my_each do |x|
+    count += 1 if yield x
+  end
+  count
+end
+if pattern.is_a? Class
+  my_each do |x|
+    return false unless x.is_a? pattern
+  end
+  true
+end
+
+# def my_map(block)
+#  result = []
+#  return to_enum unless block_given?
+#
+#  my_each(block) do |x|
+#    result.push(yield(x))
+#  end
+#  result
+# end
+
+def my_map(&block)
+  new_result = []
+  my_each do |_x|
+    new_result.push(block.call(i))
+  end
+  new_result
+end
+
+def my_inject(initial = 0, sym = :+)
+  i = 0
+  total = initial
+  while i < length
+    total = yield(total, self[i])
+    i += 1
+
+    my_each { |i| initial = initial.method(sym).call(i) } unless initial
+    total
   end
 
-  def my_each_with_index(block)
-    result = []
-    return to_enum unless block_given?
-
-    for i in 0..length
-      result.push(yield(block[i], i))
-    end
-    result
-  end
-
-  def my_select
-    result = []
-    return to_enum unless block_given?
-
-    my_each do |x|
-      output.push(x) if yield x
-    end
-    result
-  end
-
-  def my_all?
-    result = true
-    my_each do |x|
-      result = false unless yield x
-    end
-    result
-  end
-  if pattern.is_a? Class
-    my_each do |x|
-      return false unless x.is_a? pattern
-    end
-    true
-  end
-
-  def my_any?
-    result = false
-    my_each do |x|
-      result = false if yield x
-    end
-    result
-  end
-  if pattern.is_a? Class
-    my_each do |x|
-      return false unless x.is_a? pattern
-    end
-    true
-  end
-
-  def my_none?
-    result = true
-    my_each do |x|
-      result = false if yield x
-    end
-    result
-  end
-  if pattern.is_a? Class
-    my_each do |x|
-      return false unless x.is_a? pattern
-    end
-    true
-  end
-
-  def my_count
-    count = 0
-    my_each do |x|
-      count += 1 if yield x
-    end
-    count
-  end
-  if pattern.is_a? Class
-    my_each do |x|
-      return false unless x.is_a? pattern
-    end
-    true
-  end
-
-  def my_map(block)
-    result = []
-    return to_enum unless block_given?
-
-    my_each(block) do |x|
-      result.push(yield(x))
-    end
-    result
-  end
-
-  def my_map(&block)
-    new_result = []
-    self.my_each do |x|
-      new_result.push(block.call(i))
-    end
-    new_result
-  end
-
-  def my_inject(initial = 0, sym = :+)
-    i = 0
-    total = initial
-    while (i < self.length)
-      total = yield(total, self[i])
-      i = i + 1
-
-      unless initial
-        self.my_each { |i| initial = initial.method(sym).call(i) }
-
-      end
-      total
-    end
-
-    def multiply_els
-      self.my_inject(1) { |total, n| total * n }
-    end
+  def multiply_els
+    my_inject(1) { |total, n| total * n }
   end
 end
