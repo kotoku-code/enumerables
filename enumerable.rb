eb548290f94f64b92b42e794b28d94c07fb57ac4
kotoku-code <kennedy.kotoku@gmail.com>
2020-03-27 15:43:32 +0100

    fixed enumerable

--- enumerable.rb
+++ enumerable.rb
@@ -1,110 +1,111 @@
-module_Enumerable
-def my_each(block)
-  result = []
-  return to_enum unless block_given?
+module Enumerable
+  def my_each(block)
+    result = []
+    return to_enum unless block_given?
 
-  (0..length).each do |i|
-    result.push(yield(block[i]))
+    (0..length).each do |i|
+      result.push(yield(block[i]))
+    end
+    result
   end
-  result
-end
 
-def my_each_with_index(block)
-  result = []
-  return to_enum unless block_given?
+  def my_each_with_index(block)
+    result = []
+    return to_enum unless block_given?
 
-  (0..length).each do |i|
-    result.push(yield(block[i], i))
+    (0..length).each do |i|
+      result.push(yield(block[i], i))
+    end
+    result
   end
-  result
-end
 
-def my_select
-  result = []
-  return to_enum unless block_given?
+  def my_select
+    result = []
+    return to_enum unless block_given?
 
-  my_each do |x|
-    output.push(x) if yield x
+    my_each do |x|
+      output.push(x) if yield x
+    end
+    result
   end
-  result
-end
 
-def my_all?
-  result = true
-  my_each do |x|
-    result = false unless yield x
+  def my_all?
+    result = true
+    my_each do |x|
+      result = false unless yield x
+    end
+    result
   end
-  result
-end
-if pattern.is_a? Class
-  my_each do |x|
-    return false unless x.is_a? pattern
+  if pattern.is_a? Class
+    my_each do |x|
+      return false unless x.is_a? pattern
+    end
+    true
   end
-  true
-end
 
-def my_any?
-  result = false
-  my_each do |x|
-    result = false if yield x
+  def my_any?
+    result = false
+    my_each do |x|
+      result = false if yield x
+    end
+    result
   end
-  result
-end
-if pattern.is_a? Class
-  my_each do |x|
-    return false unless x.is_a? pattern
+  if pattern.is_a? Class
+    my_each do |x|
+      return false unless x.is_a? pattern
+    end
+    true
   end
-  true
-end
 
-def my_none?
-  result = true
-  my_each do |x|
-    result = false if yield x
+  def my_none?
+    result = true
+    my_each do |x|
+      result = false if yield x
+    end
+    result
   end
-  result
-end
-if pattern.is_a? Class
-  my_each do |x|
-    return false unless x.is_a? pattern
+  if pattern.is_a? Class
+    my_each do |x|
+      return false unless x.is_a? pattern
+    end
+    true
   end
-  true
-end
 
-def my_count
-  count = 0
-  my_each do |x|
-    count += 1 if yield x
+  def my_count
+    count = 0
+    my_each do |x|
+      count += 1 if yield x
+    end
+    count
   end
-  count
-end
-if pattern.is_a? Class
-  my_each do |x|
-    return false unless x.is_a? pattern
+  if pattern.is_a? Class
+    my_each do |x|
+      return false unless x.is_a? pattern
+    end
+    true
   end
-  true
-end
 
-def my_map(&block)
-  new_result = []
-  my_each do |_x|
-    new_result.push(block.call(i))
+  def my_map(&block)
+    new_result = []
+    my_each do |_x|
+      new_result.push(block.call(i))
+    end
+    new_result
   end
-  new_result
-end
 
-def my_inject(initial = 0, sym = :+)
-  i = 0
-  total = initial
-  while i < length
-    total = yield(total, self[i])
-    i += 1
+  def my_inject(initial = 0, sym = :+)
+    i = 0
+    total = initial
+    while i < length
+      total = yield(total, self[i])
+      i += 1
 
-    my_each { |item| initial = initial.method(sym).call(item) } unless initial
-    total
+      my_each { |item| initial = initial.method(sym).call(item) } unless initial
+      total
+    end
   end
-end
 
-def multiply_els
-  my_inject(1) { |total, n| total * n }
+  def multiply_els
+    my_inject(1) { |total, n| total * n }
+  end
 end
