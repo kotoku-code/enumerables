4768bc9de881bbe5112a98ba39c0877b25939539
kotoku-code <kennedy.kotoku@gmail.com>
2020-04-09 16:15:44 +0200

    creating more tests

--- testing.rb
+++ testing.rb
@@ -3,13 +3,15 @@ require_relative "enumerable"
 #p [1, 2, 3].my_map
 #p [1, 2, 3].map
 #
-#p [1, 2, 6].my_all? { |num| num < 4}
-#p [1, 2, 6].all? { |num| num < 4}
 
 
-p %w[a a].all?(String)
-p [1, 1].all? { |val| val.instance_of? Integer}
-p %w[a a].my_all?(String)
+p [1, 2, nil].all? { |num| num < 4}
+p [1, 2, nil].my_all? { |num| num < 4}
+
+
+#p %w[a a].all?(String)
+#p [1, 1].all? { |val| val.instance_of? Integer}
+#p %w[a a].my_all?(String)
 
 
 
